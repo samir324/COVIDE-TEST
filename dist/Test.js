55a7698c04253d1c88629638f551c9d1a251435f
Samir Zahidi <samirreda324@gmail.com>
2020-05-07 22:50:28 +0000

    Ajout un code pour afichier la  resultat  et des modifications

--- dist/Test.js
+++ dist/Test.js
@@ -3,6 +3,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q1',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -16,6 +17,7 @@ const questions = [{
 
         input: {
             type: 'number',
+            qNumber: 'Q2',
             name: 'degrés',
             min: 34,
             max: 42
@@ -25,6 +27,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q3',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -38,6 +41,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q4',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -51,6 +55,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q5',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -64,6 +69,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q6',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -77,6 +83,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q7',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -90,6 +97,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q8',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -103,6 +111,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q9',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -116,6 +125,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q10',
             answer: [{
                 text: 'Bien',
                 icon: ' fa fa-laugh'
@@ -135,6 +145,7 @@ const questions = [{
 
         input: {
             type: 'number',
+            qNumber: 'Q11',
             name: 'ans',
             min: 15,
             max: 110
@@ -144,6 +155,7 @@ const questions = [{
 
         input: {
             type: 'number',
+            qNumber: 'Q12',
             name: 'kg',
             min: 20,
             max: 250
@@ -153,6 +165,7 @@ const questions = [{
 
         input: {
             type: 'number',
+            qNumber: 'Q13',
             name: 'cm',
             min: 80,
             max: 250
@@ -162,6 +175,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q14',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -175,6 +189,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q15',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -188,6 +203,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q16',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -201,6 +217,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q17',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -214,6 +231,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q18',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -227,6 +245,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q19',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -240,6 +259,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q20',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -249,7 +269,6 @@ const questions = [{
             }, {
                 text: 'Homme',
                 icon: 'fa fa-male'
-
             }]
         }
     }, {
@@ -257,6 +276,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q21',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -270,6 +290,7 @@ const questions = [{
 
         input: {
             type: 'radio',
+            qNumber: 'Q22',
             answer: [{
                 text: 'Oui',
                 icon: 'fa fa-check'
@@ -289,10 +310,22 @@ const Préambule = document.querySelector('.Préambule');
 const Etape = document.querySelectorAll('.etape h1');
 const Nbrquestion = document.querySelector('.nbrquestion');
 const SuiverBar = document.querySelector('.bar');
+const nextbutton = document.querySelector('#nextbutton');
+const lastbtn = document.querySelector('.last-btn');
+const reponceQuestionPara = document.querySelector('.question');
+const reponce = document.querySelector('.reponce');
+const switsh = document.querySelector('.switsh');
+const result = document.querySelector('.Préambule h3');
 
-let i = 0;
+
+
+
+
+let i = 0;  // index de la question
+
+// botton pour  demarrer du test
 btnDemarer.addEventListener('click', function DeparTest() {
-    btnDemarer.style.display = 'none';
+    btnDemarer.style.display = 'none'; 
     Préambule.style.display = 'none';
     quicontainer.style.display = 'block';
     Etape[0].classList.remove('etape-actuelle');
@@ -303,31 +336,35 @@ btnDemarer.addEventListener('click', function DeparTest() {
 });
 
 
-const nextbutton = document.querySelector('#nextbutton');
-const lastbtn = document.querySelector('.last-btn');
-const reponceQuestionPara = document.querySelector('.question');
-const reponce = document.querySelector('.reponce');
-const switsh = document.querySelector('.switsh');
 
- // index de la question
+
+
 nextbutton.addEventListener('click', () => {
     // if()
     i++;
-    if (i >= (questions.length)-1 ) {
-        i = (questions.length)-1;
-        nextbutton.innerText= "Terminer le test";
+    if (i === (questions.length) - 1) {
+        // i = (questions.length) - 1;
+        nextbutton.innerText = "Terminer le test";
+        nextbutton.classList.add('result');
+        const resultBtn = document.querySelector('.result');
+        resultBtn.addEventListener('click', Results);
     }
     lastbtn.style.display = 'block';
     nextbutton.classList.remove('addmargin');
     nextbutton.disabled = true;
     switchQuest(questions[i]);
     suiverNbr(i);
+
+
 });
+
 function suiverNbr(j) {
-    const actlNbr = j + 1;// *car les index sont commencer par 0*
+    const actlNbr = j + 1; // *car les index sont commencer par 0*
     Nbrquestion.innerText = actlNbr; // * changer le nembre de la question *
-    SuiverBar.style.width = `calc(${actlNbr}* calc(100% / 22))`;// changer la langeur du div>.bar 
+    SuiverBar.style.width = `calc(${actlNbr}* calc(100% / 22))`; // changer la langeur du div>.bar 
 }
+
+
 
 function switchQuest(element) {
 
@@ -336,23 +373,17 @@ function switchQuest(element) {
     const reponceAnswer = element.input.answer;
     const reponceinput = element.input;
     if (reponceinput.type === 'radio') {
-
         reponceAnswer.forEach(answer => {
-
             reponce.innerHTML += `
                     <div>
-                        <input type="radio" name="choice" id="${answer.text}">
+                        <input type="radio" name="${reponceinput.qNumber}" id="${answer.text}"/>
                         <label for="${answer.text}">
                         <i class="fas ${answer.icon}"></i>
                         <span>${answer.text}</span> </label>
                     </div>`;
         });
-
-
-
     } else {
-
-        reponce.innerHTML += `<input type="number"  id="${reponceinput.name}" min="${reponceinput.min}" max="${reponceinput.max}" placeholder="${reponceinput.min} - ${reponceinput.max}">
+        reponce.innerHTML += `<input type="number"  id="${reponceinput.name}" name="${reponceinput.qNumber}" min="${reponceinput.min}" max="${reponceinput.max}" placeholder="${reponceinput.min} - ${reponceinput.max}"/>
                                     <span class="input-span">${reponceinput.name}</span>`
     }
 
@@ -360,14 +391,16 @@ function switchQuest(element) {
 };
 lastbtn.addEventListener('click', () => {
     i--;
-    nextbutton.innerText= "suivant";
-    if (i <= 0){
+    nextbutton.innerText = "suivant";
+    if (i <= 0) {
         i = 0;
         nextbutton.classList.add('addmargin');
         lastbtn.style.display = 'none';
-    } 
+    }
     switchQuest(questions[i]);
     suiverNbr(i);
+
+    nextbutton.classList.remove('result')
 });
 
 switsh.addEventListener('change', (event) => {
@@ -375,12 +408,62 @@ switsh.addEventListener('change', (event) => {
     if (input.type === 'number') {
         const testnumber = parseFloat(input.value);
         if (testnumber >= input.min && testnumber <= input.max) {
+            values[input.name] = input.value;
             nextbutton.disabled = false;
         } else {
             nextbutton.disabled = true;
         }
     } else {
+        values[input.name] = input.id;
+        console.log(values);
         nextbutton.disabled = false;
     }
 });
- 
+
+
+const resultPara = document.querySelectorAll('.Préambule p');
+
+console.log(resultPara[0]);
+
+let values = {};
+let gravité = 0;
+
+function Results() {
+    if (values['Q1'] === 'Oui') {  gravité++   }
+    if (values['Q8'] === 'Oui' || values['Q9'] === 'Oui') {   gravité++   }
+    if (values['Q10'] === 'Fatigué(e)' || values['Q10'] === 'Très fatigué') {  gravité++  }
+    if (values['Q14'] === 'Oui' || values['Q15'] === 'Oui') {  gravité++   }
+    AffichageResult(gravité)
+}
+
+function AffichageResult(gravité) {
+    Etape[1].classList.remove('etape-actuelle');
+    Etape[2].classList.add('etape-actuelle');
+    btnDemarer.style.display = 'block';
+    Préambule.style.display = 'block';
+    quicontainer.style.display = 'none';
+    btnDemarer.textContent = ' Recommencer le test';
+    
+    btnDemarer.addEventListener('click', () => {
+        window.location.reload();
+    });
+
+    result.innerText = 'Résultats';
+    if (gravité === 0) {
+        resultPara[0].innerText = 'Votre situation ne relève probablement pas du Covid-19. N’hésitez pas à contacter votre médecin en cas de doute. Vous pouvez refaire le test en cas de nouveau symptôme pour réévaluer la situation. Pour toute information concernant le Covid-19, consulter la page Conseils'
+        resultPara[1].innerText = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+    } else if (gravité === 1) {
+        resultPara[0].innerText = 'Nous vous conseillons de rester à votre domicile et de contacter votre médecin en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouveau l’application pour réévaluer vos symptômes'
+        resultPara[1].innerText = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+    } else if (gravité === 2) {
+        resultPara[0].innerText = "Vous pouvez faire une téléconsultation ou médecin généraliste ou visite à domicile. Appelez le 141 si une gêne respiratoire ou des difficultés importantes pour s’alimenter ou boire pendant plus de 24h apparaissent."
+        resultPara[1].innerText = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+    } else {
+        resultPara[0].innerText = "Appelez le 141"
+        resultPara[1].innerText = 'Restez chez vous au maximum en attendant que les symptômes disparaissent. Prenez votre température deux fois par jour. Rappel des mesures d’hygiène.'
+        resultPara[0].style.color = '#d63031';
+        resultPara[0].style.fontSize = '50px';
+        resultPara[0].style.fontWeight = 'bold';
+    }
+
+}
